b921d98cd652a7cdba7b9a4ed70ae54b14068970
Jonathan Richer dit Laflèche <jonathanr@fyrsticycle.ca>
2019-03-01 20:32:14 -0500

    update windows build script

--- build_windows.sh
+++ build_windows.sh
@@ -5,3 +5,10 @@ i686-w64-mingw32-g++ src/NamourCounter.cpp -o Release-32bit-Windows/NamourCounte
 echo "Compiling 64bit Windows Release"
 x86_64-w64-mingw32-g++ src/NamourCounter.cpp -o Release-64bit-Windows/NamourCounter.exe
 echo "Compiling success!"
+
+echo "Compressing Release Dirs"
+zip Release-32bit-Windows Release-32bit-Windows/*
+zip Release-64bit-Windows Release-64bit-Windows/*
+echo "Compressing success!"
+
+echo "\n exiting ...\n"
